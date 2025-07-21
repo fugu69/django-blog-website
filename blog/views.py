5895b1fcfc5f507b324ec6900b70d35996afb0e3
Maximus Brutalis <primeskobelev@gmail.com>
2025-07-21 14:23:44 +0700

    views(post): add post details logic

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, "home.html", {"posts": posts})
 
 
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_details.html", {"post": post})
