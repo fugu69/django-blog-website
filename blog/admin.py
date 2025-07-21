e1c6b51d747deb5874e705c49393f6e272c95a5e
Maximus Brutalis <primeskobelev@gmail.com>
2025-07-21 12:57:11 +0700

    feat(admin): add PostAdmin for improved admin list view

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+
+
+admin.site.register(Post, PostAdmin)
