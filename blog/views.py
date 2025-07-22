dd08dbfa9d0fa8bf917b9f3275a8a629a0c190b4
Maximus Brutalis <primeskobelev@gmail.com>
2025-07-22 14:23:04 +0700

    feat(views): Add view for post creation

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -10,3 +11,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_details.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
